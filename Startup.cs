32cf0fba1c4464de059bc4ddfa0786dbf1cc5807
César <paulo1988cesar@msn.com>
2019-01-16 18:15:15 -0200

    Finalizando a criação de erros personalizados.

--- Startup.cs
+++ Startup.cs
@@ -15,6 +15,10 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using System.Net;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using DatingApp.API.Helpers;
 
 namespace DatingApp.API
 {
@@ -58,6 +62,19 @@ namespace DatingApp.API
             }
             else
             {
+                app.UseExceptionHandler(builder => 
+                {builder.Run(async context => {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                        var error = context.Features.Get<IExceptionHandlerFeature>();
+
+                        if (error != null)
+                        {
+                            context.Response.AddApplicationError(error.Error.Message);
+                            await context.Response.WriteAsync(error.Error.Message);
+                        }
+                    });
+                });
                 //app.UseHsts();
             }
 
