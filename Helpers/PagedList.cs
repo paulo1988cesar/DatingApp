25c87b66e49be1ff72771eeabfa03993b553b3ba
Paulo César <paulo1988cesar@msn.com>
2019-04-11 15:42:32 -0300

    Alterando espaços entre linhas

--- Helpers/PagedList.cs
+++ Helpers/PagedList.cs
@@ -22,13 +22,11 @@ namespace DatingApp.API.Helpers
             this.AddRange(items);
         }
 
-        public static async Task<PagedList<T>> CreateAsync(IQueryable<T> source, 
-                            int pageNumber, int pageSize)
+        public static async Task<PagedList<T>> CreateAsync(IQueryable<T> source, int pageNumber, int pageSize)
         {
             var count = await source.CountAsync();   
             var items = await source.Skip((pageNumber -1) * pageSize).Take(pageSize).ToListAsync();
             return new PagedList<T>(items, count, pageNumber, pageSize);             
         }
-
     }
 }
